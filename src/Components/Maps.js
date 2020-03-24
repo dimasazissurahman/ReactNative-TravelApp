d971a78382962a19f99f6f28f246b06b97b6bb0e
Anthoni Setiawan (ID) <anthoni.setiawan@Toni-Ganteng.local>
2020-03-24 21:52:13 +0700

    post data backend

--- src/Components/Maps.js
+++ src/Components/Maps.js
@@ -1,88 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image, ActivityIndicator } from 'react-native';
-import MapView, { Marker } from 'react-native-maps'
-import styled from 'styled-components';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
-import peopleIcon from '../../assets/peopleIcon.png';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  ActivityIndicator
+} from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import styled from "styled-components";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import peopleIcon from "../../assets/peopleIcon.png";
 
 const Container = styled.View`
   flex-grow: 1;
 `;
 
 function Maps({ getLoc, onPress }) {
-    const [textLatitude, setTextLatitude] = useState(0);
-    const [textLongitude, setTextLongitude] = useState(0);
-    const [location, setLocation] = useState();
-    const [errMsg, setErrMsg] = useState();
-    
+  const [textLatitude, setTextLatitude] = useState(0);
+  const [textLongitude, setTextLongitude] = useState(0);
+  const [location, setLocation] = useState();
+  const [errMsg, setErrMsg] = useState();
 
-    const getLocationAsync = async () => {
-        let status = await Permissions.askAsync(Permissions.LOCATION);
+  const getLocationAsync = async () => {
+    let status = await Permissions.askAsync(Permissions.LOCATION);
 
-        if (status.status !== 'granted') {
-            setErrMsg('Permission to access location was denied');
-        } else {
-            setErrMsg('Permssion granted')
+    if (status.status !== "granted") {
+      setErrMsg("Permission to access location was denied");
+    } else {
+      setErrMsg("Permssion granted");
 
-            let currlocation = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
-            console.log("jalan");
-            
-            setLocation(currlocation);
-            setTextLatitude(currlocation.coords.latitude);
-            setTextLongitude(currlocation.coords.longitude);
-        }
+      let currlocation = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.High
+      });
+
+      setLocation(currlocation);
+      setTextLatitude(currlocation.coords.latitude);
+      setTextLongitude(currlocation.coords.longitude);
     }
-    const region = {
-        latitude: textLatitude,
-        longitude: textLongitude,
-        latitudeDelta: 0.0043,
-        longitudeDelta: 0.0034
-    }
-    useEffect(() => {
-        getLocationAsync();
-    }, [getLoc]);
-    console.log(location);
+  };
+  const region = {
+    latitude: textLatitude,
+    longitude: textLongitude,
+    latitudeDelta: 0.0043,
+    longitudeDelta: 0.0034
+  };
+  useEffect(() => {
+    getLocationAsync();
+  }, [getLoc]);
 
-    console.log(textLatitude);
-    console.log(textLongitude);
-    console.log(errMsg);
-
-
-
-
-    return (
-        <Container>
-            {textLatitude ?
-                <MapView
-                    style={{ flex: 1 }}
-                    region={region}
-                >
-                    <Marker
-                        coordinate={{
-                            latitude: textLatitude,
-                            longitude: textLongitude
-                        }}
-                    >
-                        <Image style={{ height: 30, width: 30 }} resizeMode={"contain"} source={peopleIcon} />
-                    </Marker>
-                    <Marker
-                        coordinate={{ latitude: -6.1770648, longitude: 106.6462424 }}
-                    >
-                        <Image style={{ height: 30, width: 30 }} resizeMode={"contain"} source={peopleIcon} />
-                    </Marker>
-                    <Marker
-                        coordinate={{ latitude: -6.1760640, longitude: 106.6465423 }}
-                        onPress={onPress}
-                    >
-                        <Image style={{ height: 30, width: 30 }} resizeMode={"contain"} source={peopleIcon} />
-                    </Marker>
-                </MapView>
-                :
-                <ActivityIndicator style={{ flex: 1, alignSelf: "center" }} size={"large"} animating />
-            }
-        </Container>
-    );
+  return (
+    <Container>
+      {textLatitude ? (
+        <MapView style={{ flex: 1 }} region={region}>
+          <Marker
+            coordinate={{
+              latitude: textLatitude,
+              longitude: textLongitude
+            }}
+          >
+            <Image
+              style={{ height: 30, width: 30 }}
+              resizeMode={"contain"}
+              source={peopleIcon}
+            />
+          </Marker>
+          <Marker coordinate={{ latitude: -6.1770648, longitude: 106.6462424 }}>
+            <Image
+              style={{ height: 30, width: 30 }}
+              resizeMode={"contain"}
+              source={peopleIcon}
+            />
+          </Marker>
+          <Marker
+            coordinate={{ latitude: -6.176064, longitude: 106.6465423 }}
+            onPress={onPress}
+          >
+            <Image
+              style={{ height: 30, width: 30 }}
+              resizeMode={"contain"}
+              source={peopleIcon}
+            />
+          </Marker>
+        </MapView>
+      ) : (
+        <ActivityIndicator
+          style={{ flex: 1, alignSelf: "center" }}
+          size={"large"}
+          animating
+        />
+      )}
+    </Container>
+  );
 }
 
 export default Maps;
